903f3175cc4b5ebec91caeb4ff4cf6b7c8de0eaf
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2025-01-31 11:51:04 +0100

    3.-4. feladat - Dénes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\NevnapController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/apiminta/nevnapok/?nap={datum}',[NevnapController::class,'index']);
