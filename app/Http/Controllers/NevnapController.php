e1a0234b4942d11e1685208039ec16aefd023e9c
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2025-02-06 15:47:47 +0100

    Kelen: 6. feladat

--- app/Http/Controllers/NevnapController.php
+++ app/Http/Controllers/NevnapController.php
@@ -4,18 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Nevnap;
+use Log;
 
 class NevnapController extends Controller
 {
     function index(Request $request)
     {
-        $ho = explode('-',$request->get('nap'))[0];
-        $nap = explode('-',$request->get('nap'))[1];
+        if ($request->has('nap'))
+        {
+            $ho = explode('-',$request->get('nap'))[0];
+            $nap = explode('-',$request->get('nap'))[1];
+    
+            $data = Nevnap::where('ho',$ho)->where('nap',$nap)->get();
+            $response = response()->json($data);
+            $response->headers->set('Access-Control-Allow-Origin','*');
+    
+            return $response;
+        }
+        else if ($request->has('nev'))
+        {
+            $nev = $request->get('nev');
+            $data = Nevnap::where('nev1', $nev)->orWhere('nev2', $nev)->get();
 
-        $data = Nevnap::where('ho',$ho)->where('nap',$nap)->get();
-        $response = response()->json($data);
-        $response->headers->set('Access-Control-Allow-Origin','*');
-
-        return $response;
+            // setlocale(LC_ALL, "hu_HU.UTF8");
+            // $data->ho = strftime("%m", mktime($data->ho));
+            $response = response()->json($data);
+            $response->headers->set('Access-Control-Allow-Origin','*');
+    
+            return $response;
+        }
     }
 }
